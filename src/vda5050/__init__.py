6865c2248e2409f2d5424603164ca8379ffd4ead
Tinapat Game Limsila <limsila.limsila@gmail.com>
2026-07-09 16:21:45 +0800

    feat: add client adapter and pybind bindings for the vda5050 wheel
    Expose core types via _core and wire scikit-build to build/link the
    adapter-backed extension module.
    
    Signed-off-by: Game TL <tinapatgl@a-star.edu.sg>

--- src/vda5050/__init__.py
+++ src/vda5050/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
-from ._core import __doc__, __version__
+from ._core import __doc__, __version__, ActionStatus
+# from . import ActionStatus
 
-__all__ = ["__doc__", "__version__"]
+__all__ = ["__doc__", "__version__","add","ActionStatus"]
